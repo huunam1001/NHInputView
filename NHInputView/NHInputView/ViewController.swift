aa0321b29ea83065c4219ff18e51b0dd00c79f6b
ninh nam <huunam101@gmail.com>
2020-04-28 14:50:34 +0700

    First commit to git

---
+++ NHInputView/NHInputView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NHInputView
+//
+//  Created by ninh nam on 4/28/20.
+//  Copyright © 2020 ninh nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NHInputView/NHInputViewTests/NHInputViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  NHInputViewTests.swift
+//  NHInputViewTests
+//
+//  Created by ninh nam on 4/28/20.
+//  Copyright © 2020 ninh nam. All rights reserved.
+//
+
+import XCTest
+@testable import NHInputView
+
+class NHInputViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
